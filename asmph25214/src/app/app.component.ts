01babf03d37cce2eec1bf23ac119329edae409ad
duongnguyen1112 <duongnguyenoja1112@gmail.com>
2024-06-12 00:55:45 +0700

    init project

---
+++ asmph25214/src/app/app.component.ts
@@ -0,0 +1,36 @@
+
+import { Component } from '@angular/core'; //import component
+import { RouterLink, RouterOutlet, Router } from '@angular/router';
+import { FormsModule } from '@angular/forms'; //import FormsModule
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet,RouterLink,FormsModule],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+
+//export class TenComponent
+export class AppComponent {
+  title = 'Duongnvph25214';
+  info = {
+    name: 'duongnvph25214',
+    email: 'duongnvph25214@fpt.edu.vn',
+    gender: 'male',
+  }
+  name='';
+  count = 0;
+  //khai báo hàm:
+  //khai báo kiểu dữ liệu trả về: tenHam(): kieuDulieu {}
+  //ko khai báo kiểu dữ liệu trả về: tenHam() {}
+  countClick(): void {
+    this.count = this.count + 1; //tăng giá trị của count lên 1 đơn vị
+  }
+
+  logout(): void {
+    const userinfo = localStorage.getItem('user');
+    if (userinfo !== null) { //đang tồn tại biến user trong localStorage
+      localStorage.removeItem('user'); //xóa user khỏi localStorage-> kết thúc phiên đăng nhập
+    }
+  }
+}
